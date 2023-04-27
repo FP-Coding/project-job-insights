8ee5e79f2f0e0ac2449844d0a4e690be3e166f05
Felipe Pinto <felipe14062000@gmail.com>
2023-04-27 16:09:03 -0300

    feat: implementando a função filter_by_job_type e adicionando try except nas outras duas funções do arquivo

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -8,32 +8,28 @@ PATH = "./data/jobs.csv"
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    with open(path, encoding="utf-8") as file:
-        reader = csv.reader(file, delimiter=",", quotechar='"')
-        fields, *jobs = reader
-        list_jobs = [dict(zip(fields, job)) for job in jobs]
-        return list_jobs
+    try:
+        with open(path, encoding="utf-8") as file:
+            fields, *jobs = csv.reader(file, delimiter=",", quotechar='"')
+            list_jobs = [dict(zip(fields, job)) for job in jobs]
+            return list_jobs
+    except FileNotFoundError:
+        return []
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    list_jobs = read(path)
-    job_types = {job["job_type"] for job in list_jobs}
-    return job_types
+    try:
+        list_jobs = read(path)
+        if len(list_jobs) == 0:
+            raise FileNotFoundError
+        job_types = {job["job_type"] for job in list_jobs}
+        return job_types
+    except FileNotFoundError:
+        return {}
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    filteredByType = list(
+        filter((lambda job: job["job_type"] == job_type), jobs)
+    )
+    return filteredByType
