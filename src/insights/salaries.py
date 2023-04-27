45fd377af109dca59004b6182fcc7176b511bcfc
Felipe Pinto <felipe14062000@gmail.com>
2023-04-27 20:32:06 -0300

    feat: implementação das funções filter_by_salary_range e matches_salary_range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -43,51 +43,25 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
+    try:
+        min = int(job["min_salary"])
+        max = int(job["max_salary"])
+        if (min > max):
+            raise ValueError
+        return min <= int(salary) <= max
+    except (KeyError, TypeError):
+        raise ValueError
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+def filter_condition(job, salary):
+    try:
+        return matches_salary_range(job, salary)
+    except (ValueError):
+        return False
 
 
 def filter_by_salary_range(
     jobs: List[dict],
     salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    raise NotImplementedError
-
-
-print(get_max_salary(PATH))
-print(get_min_salary(PATH))
+    return list(filter((lambda job: filter_condition(job, salary)), jobs))
