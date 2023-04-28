5206aaf21b0dcf3bafbf0717156b9d73bb57b17d
Felipe Pinto <felipe14062000@gmail.com>
2023-04-27 21:23:16 -0300

    test: testando a função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    report = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert "title" in report[0]
+    assert "salary" in report[0]
+    assert "type" in report[0]
+    assert "tipo" not in report[0]
+    assert "salario" not in report[0]
+    assert "titulo" not in report[0]
